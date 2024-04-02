dd8eeb2f1501a118b317fd06c119e29f6278a7b6
gustavoriquelopes <85792950+gustavoriquelopes@users.noreply.github.com>
2024-04-02 10:03:15 -0300

    upload

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;         //essa linha
+using System.ComponentModel.DataAnnotations.Schema;  // essa linha
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]                 //essa linha
+    [DataType(DataType.Date)]                        //essa linha
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]           //essa linha
+    public decimal Price { get; set; }
+}
